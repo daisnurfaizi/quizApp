aafcfa95439823c0e27768797668412282566e36
daisnurfaizi <dsnurfaizi@gmail.com>
2024-04-20 23:38:33 +0700

    remode:unused code in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,3 @@ import 'package:quiz/quiz.dart';
 void main() {
   runApp(const Quiz());
 }
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
